1d6aabb690345f48f860ae986eee30ad51c11e42
jieqiboh <jieqiboh5836@gmail.com>
2026-04-14 15:35:13 +0800

    chore: scaffold astropaper

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://jieqiboh.me/",
+  author: "jieqiboh",
+  profile: "https://github.com/jieqiboh",
+  desc: "jieqi's archive",
+  title: "jieqi's archive",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000,
+  showArchives: true,
+  showBackButton: true,
+  editPost: {
+    enabled: false,
+    text: "Edit page",
+    url: "https://github.com/jieqiboh/jieqiboh.github.io/edit/main/src/data/blog/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr",
+  lang: "en",
+  timezone: "Asia/Singapore",
+} as const;
